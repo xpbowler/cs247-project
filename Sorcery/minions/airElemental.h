d42e22b8a9c4c2c12427cc0631ffc30d81202c63
cyiuhim <cyiuhim@gmail.com>
2025-07-23 21:09:32 -0400

    implemented part of minions

---
+++ Sorcery/minions/airElemental.h
@@ -0,0 +1,14 @@
+#ifndef __AIR_ELEMENTAL_H__
+#define __AIR_ELEMENTAL_H__
+
+#include "baseMinion.h"
+
+class AirElemental : public BaseMinion {
+
+public: 
+    void useSkill() override;
+
+};
+
+#endif 
+
---
+++ Sorcery/minions/earthElemental.h
@@ -0,0 +1,13 @@
+#ifndef __EARTH_ELEMENTAL_H__
+#define __EARTH_ELEMENTAL_H__
+
+#include "baseMinion.h"
+
+class EarthElemental : public BaseMinion {
+
+public: 
+    void useSkill() override;
+};
+
+#endif 
+
