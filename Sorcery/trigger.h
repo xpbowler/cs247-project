9f623351c35caecec45b68651615a7e795e25c16
Ryan Nguyen <ryan.nguyen@uwaterloo.ca>
2025-07-23 21:08:56 -0400

    .

---
+++ Sorcery/trigger.h
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Card;
+
+class TriggerTopic {
+    std::vector<Trigger*> observers;
+    public:
+        void notifyTriggers();
+        void attachTrigger(Trigger* trigger);
+        void detachTrigger(Trigger* trigger);
+};
+
+class Trigger {
+    TriggerTopic* triggerTopic;
+    Card* owner;
+    public:
+        Trigger();
+        void notifyOwner();
+};
