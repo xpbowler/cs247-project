c626c86d0b382a91712305089f82e8c65c7371a7
Jeff Mak <e_jeffmak@hotmail.com>
2025-07-26 19:12:06 -0400

    Deck initialization

--- Sorcery/player.cc
+++ Sorcery/player.cc
@@ -1,5 +1,23 @@
 #include "player.h"
-
+#include "airElemental.h"
+#include "earthElemental.h"
+#include "fireElemental.h"
+#include "potionSeller.h"
+#include "novicePyromancer.h"
+#include "apprenticeSummoner.h"
+#include "masterSummoner.h"
+#include "banish.h"
+#include "unsummon.h"
+#include "recharge.h"
+#include "disenchant.h"
+#include "raiseDead.h"
+#include "blizzard.h"
+#include "giantStrength.h"
+#include "magicFatigue.h"
+#include "silence.h"
+#include "darkRitual.h"
+#include "auraOfPower.h"
+#include "standstill.h"
 #include <iostream>
 #include <fstream>
 
@@ -34,8 +52,46 @@ void Player::initializeDeck(const string& deckFilePath) {
         throw runtime_error("Failed to open deck initialization file: " + deckFilePath);
     }
     string card_string;
+    this->deck = vector<unique_ptr<Card>>();
     while (getline(initFile, card_string)) {
-
-        // deck.push_back(card);
+        if (card_string == "Air Elemental") {
+            deck.push_back(make_unique<Card>(make_unique<AirElemental>()));
+        } else if (card_string == "Earth Elemental") {
+            deck.push_back(make_unique<Card>(make_unique<EarthElemental>()));
+        } else if (card_string == "Fire Elemental") {
+            deck.push_back(make_unique<Card>(make_unique<FireElemental>()));
+        } else if (card_string == "Potion Seller") {
+            deck.push_back(make_unique<Card>(make_unique<PotionSeller>()));
+        } else if (card_string == "Novice Pyromancer") {
+            deck.push_back(make_unique<Card>(make_unique<NovicePyromancer>()));
+        } else if (card_string == "Apprentice Summoner") {
+            deck.push_back(make_unique<Card>(make_unique<ApprenticeSummoner>()));
+        } else if (card_string == "Master Summoner") {
+            deck.push_back(make_unique<Card>(make_unique<MasterSummoner>()));
+        } else if (card_string == "Banish") {
+            deck.push_back(make_unique<Card>(make_unique<Banish>()));
+        } else if (card_string == "Unsummon") {
+            deck.push_back(make_unique<Card>(make_unique<Unsummon>()));
+        } else if (card_string == "Recharge") {
+            deck.push_back(make_unique<Card>(make_unique<Recharge>()));
+        } else if (card_string == "Disenchant") {
+            deck.push_back(make_unique<Card>(make_unique<Disenchant>()));
+        } else if (card_string == "Raise Dead") {
+            deck.push_back(make_unique<Card>(make_unique<RaiseDead>()));
+        } else if (card_string == "Blizzard") {
+            deck.push_back(make_unique<Card>(make_unique<Blizzard>()));
+        } else if (card_string == "Giant Strength") {
+            deck.push_back(make_unique<Card>(make_unique<GiantStrength>()));
+        } else if (card_string == "Magic Fatigue") {
+            deck.push_back(make_unique<Card>(make_unique<MagicFatigue>()));
+        } else if (card_string == "Silence") {
+            deck.push_back(make_unique<Card>(make_unique<Silence>()));
+        } else if (card_string == "Dark Ritual") {
+            deck.push_back(make_unique<Card>(make_unique<DarkRitual>()));
+        } else if (card_string == "Aura of Power") {
+            deck.push_back(make_unique<Card>(make_unique<AuraOfPower>()));
+        } else if (card_string == "Standstill") {
+            deck.push_back(make_unique<Card>(make_unique<Standstill>()));
+        }
     }
 }
