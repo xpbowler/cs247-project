3500967bf67024702c1b706c67ed72153a61b96d
cyiuhim <cyiuhim@gmail.com>
2025-07-26 21:44:55 -0400

    minion interface

--- Sorcery/minions/minion.h
+++ Sorcery/minions/minion.h
@@ -1,19 +1,39 @@
 #pragma once 
 
 #include <card.h>
+#include <memory>
+#include <util.h>
+
+class BaseEnchantment;
 
 class Minion : public Card {
 
 public: 
     Minion(Player& owner, Player& opponent);
 
-// TODO: implement rest of the public and protected fields for this
+    int getDefence();
+    int getAttack();
+    void setAttack(int attack);
+    void heal(int hp);
+    void takeDamage(int dmg);
+    void attackMinion(Minion* minion);
+    void attackPlayer(int dmg);
+    void gainStats(int attack, int defence);
+    void summonMinion(MinionType minionType, int amount);
+
 // no need to put useSkill here, it is already in the intermediate subclasses
 
 protected: 
 
     virtual ~Minion() = 0;
 
+    int attack;
+    int defence;
+    int actions;
+    MinionType minionType;
+    std::unique_ptr<BaseEnchantment> enchantment;
+    std::string name;
+    
 };
 
   
